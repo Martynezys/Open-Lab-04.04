cc5b052344c6565082fd85e2bd797043e9bc6421
Martynezys <lenlein@proton.me>
2022-10-20 08:58:43 +0200

    add -- function that coverts int to phone number

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Open_Lab_04._04
 {
@@ -6,7 +7,9 @@ namespace Open_Lab_04._04
     {
         public string FormatPhoneNumber(int[] numbers)
         {
-            throw new NotImplementedException();
+            string numeros = "(" + $"{numbers[0]}{numbers[1]}{ numbers[2]}" + ")" + " " + $"{numbers[3]}{numbers[4]}{numbers[5]}" + "-" + $"{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+            return numeros;
         }
+        
     }
 }
